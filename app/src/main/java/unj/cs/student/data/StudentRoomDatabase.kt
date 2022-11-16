f0ddab215628773492b919ca7c8b25744492d7b0
Adnif <ahmadbani9@gmail.com>
2022-11-16 12:38:42 +0700

    quiz

--- app/src/main/java/unj/cs/student/data/StudentRoomDatabase.kt
+++ app/src/main/java/unj/cs/student/data/StudentRoomDatabase.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.*
 
-@Database(entities = [Student::class], version = 1, exportSchema = false)
+@Database(entities = [Student::class], version = 2, exportSchema = false)
 abstract class StudentRoomDatabase : RoomDatabase() {
     abstract fun studentDao() : StudentDao
 
